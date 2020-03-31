51e843c67b5dc8882163a77e80297caf09c5df53
Lee Painter <lee@ludup.com>
2020-03-31 12:06:04 +0100

    Commented MobileAgent.java

--- desktop-ssh-agent/src/main/java/com/sshtools/mobile/agent/MobileAgent.java
+++ desktop-ssh-agent/src/main/java/com/sshtools/mobile/agent/MobileAgent.java
@@ -6,8 +6,20 @@ import org.eclipse.swt.widgets.Display;
 
 import com.sshtools.desktop.agent.DesktopAgent;
 
+/**
+ * This class extends DesktopAgent for compatibility with older versions.
+ * @author lee
+ *
+ */
 public class MobileAgent extends DesktopAgent {
 
+	/**
+	 * 
+	 * @param display
+	 * @param restartCallback
+	 * @param shutdownCallback
+	 * @throws IOException
+	 */
 	MobileAgent(Display display, Runnable restartCallback, Runnable shutdownCallback) throws IOException {
 		super(display, restartCallback, shutdownCallback);
 	}
